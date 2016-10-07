023002ca4a485321052a445fb48050de8ecc8712
Bohdan-Ivan Maliuha <bogdan.maliuga@gmail.com>
2016-10-07 06:53:35 +0300

    0653

--- js/app.js
+++ js/app.js
@@ -1,301 +1,57 @@
 angular.module('myApp', ['angularModalService', 'ngAnimate'])
     .controller('myCtrl', ["$scope", "$http", "ModalService", function($scope, $http, ModalService) {
-
-        var reqGet = {
+        //GET DATA
+        $http({
             method: 'GET',
             url: 'https://easy-energy.herokuapp.com/a/electricityOffer/1.json',
             headers: {
                 'Content-Type': 'application/json;charset=utf-8'
             }
+        }).then(function(response) {
+            $scope.content = response.data;
+            convertDate();
 
-        };
-        var reqPostCalculation = {
-            method: 'POST',
-            dataType: 'json',
-            url: 'https://easy-energy.herokuapp.com/a/CalculateElectricityOffer',
-            data: $scope.content,
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        };
-        var reqPostSave = {
-            method: 'POST',
-            dataType: 'json',
-            url: 'https://easy-energy.herokuapp.com/a/electricityOffer.json',
-            data: $scope.content,
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        };
+        });
 
-        $http(reqGet)
-            .then(function(response) {
-                $scope.content = response.data;
-                convertDate();
-
-            });
-
-
+        //POST DATA
         $scope.sendData = function() {
-            $scope.content.offerStorageId=null;
+            $scope.content.offerStorageId = null;
             convertDateFromObject();
-            console.log($scope.content);
-
 
             $http({
-              method: 'POST',
-              dataType: 'json',
-              url: 'https://easy-energy.herokuapp.com/a/electricityOffer.json',
-              data:   {
-                    "offerNumber": 1,
-                    "creationDate": 1473325469377,
-                    "lastEditionDate": 1473325469377,
-                    "companyDTO": {
-                        "id": null,
-                        "companyName": "Hotel pod Dębami",
-                        "companyCode": "Hotel pod Dębami",
-                        "nip": "5641713529",
-                        "regon": null,
-                        "zipCode": "20-706",
-                        "city": "Wrzosy",
-                        "streetName": "Wysoki dołki",
-                        "streetNumber": "4"
-                    },
-                    "receiverPointList": [
-
-                        {
-                            "receiverPointDescription": "Licznik numer 894837",
-                            "tariffCode": "Taryfa Popołudniowa",
-                            "actualNumberOfZones": 2,
-                            "actualZoneList": [{
-                                "actualZoneCodeCode": "Strefa A1"
-                            }, {
-                                "actualZoneCodeCode": "Strefa C2"
-                            }],
-                            "invoiceList": [{
-                                "orderNumber": null,
-                                "documentNumber": "FA 22/08/2016",
-                                "periodStart": 1453248000000,
-                                "getPeriodStop": 1464134400000,
-                                "invoiceZoneConsumptionList": [{
-                                    "actualZoneCode": "Strefa A1",
-                                    "unitConsumption": 300
-                                }, {
-                                    "actualZoneCode": "Strefa C2",
-                                    "unitConsumption": 200
-                                }]
-                            }, {
-                                "orderNumber": null,
-                                "documentNumber": "FA 188/10/2016",
-                                "periodStart": 1457568000000,
-                                "getPeriodStop": 1462838400000,
-                                "invoiceZoneConsumptionList": [{
-                                    "actualZoneCode": "Strefa A1",
-                                    "unitConsumption": 300
-                                }, {
-                                    "actualZoneCode": "Strefa C2",
-                                    "unitConsumption": 200
-                                }]
-                            }],
-                            "receiverPointOfferCalculation": {
-                                "totalConsumptionSummary": {
-                                    "totalNumberOfDaysForAllPeriods": 60,
-                                    "totalElectricityUnitsConsumptionInAllPeriods": 1000,
-                                    "predictedElectricityUnitConsumptionPerYear": 6000
-                                },
-                                "actualReceiverPointFees": {
-                                    "actualTradeFee": 15,
-                                    "actualZoneFeeList": [{
-                                        "actualZoneCode": "Strefa A1",
-                                        "actualUnitPrice": 200
-                                    }, {
-                                        "actualZoneCode": "Strefa C2",
-                                        "actualUnitPrice": 300
-                                    }]
-                                },
-                                "offerParameters": {
-                                    "proposalContractMonthLength": 24,
-                                    "defaultProposalTradeFee": 12,
-                                    "defaultZoneParamsList": [{
-                                        "actualZoneCode": "Strefa A1",
-                                        "defaultUnitPrice": 150
-                                    }, {
-                                        "actualZoneCode": "Strefa C2",
-                                        "defaultUnitPrice": 250
-                                    }],
-                                    "defaultZoneCodesSameAsActual": false
-                                },
-                                "proposalSellerList": [{
-                                    "sellerCode": "Tauron",
-                                    "proposalTradeFee": 12,
-                                    "sellerTariffPublicationDate": 1473325469349,
-                                    "proposalZoneDetailsList": [{
-                                        "actualZoneCode": "Strefa A1",
-                                        "sellerMinimalUnitPrice": 150,
-                                        "proposalUnitPrice": 180,
-                                        "proposalZoneCode": "Strefa A1 od Tauron"
-                                    }, {
-                                        "actualZoneCode": "Strefa C2",
-                                        "sellerMinimalUnitPrice": 150,
-                                        "proposalUnitPrice": 280,
-                                        "proposalZoneCode": "Strefa C2 od Tauron"
-                                    }],
-                                    "receiverPointEstimation": {
-                                        "sellerCode": "Tauron",
-                                        "receiverPointDataEstimation": {
-                                            "tariffIssueDate": 1473325469350,
-                                            "estimatedUnitConsumptionInYearScale": 99999,
-                                            "estimatedContractProfitValue": 5000,
-                                            "estimatedContractProfitValueInYearScale": 2500,
-                                            "estimatedSavingsInYearScale": 600,
-                                            "estimatedSavingsInContractScale": 1200,
-                                            "estimatedSavingsInPercentage": 30
-                                        },
-                                        "receiverPointProvisionList": [{
-                                            "levelCode": "Próg I",
-                                            "traderProvisionInYearScale": 300,
-                                            "traderProvisionInContractScale": 600,
-                                            "partnerProvisionInContractScale": 150
-                                        }, {
-                                            "levelCode": "Próg II",
-                                            "traderProvisionInYearScale": 400,
-                                            "traderProvisionInContractScale": 800,
-                                            "partnerProvisionInContractScale": 200
-                                        }, {
-                                            "levelCode": "Próg III",
-                                            "traderProvisionInYearScale": 500,
-                                            "traderProvisionInContractScale": 1000,
-                                            "partnerProvisionInContractScale": 250
-                                        }]
-                                    }
-                                }, {
-                                    "sellerCode": "Cez",
-                                    "proposalTradeFee": 13,
-                                    "sellerTariffPublicationDate": 1473325469351,
-                                    "proposalZoneDetailsList": [{
-                                        "actualZoneCode": "Strefa A1",
-                                        "sellerMinimalUnitPrice": 110,
-                                        "proposalUnitPrice": 175,
-                                        "proposalZoneCode": "Strefa A1 od CEZ"
-                                    }, {
-                                        "actualZoneCode": "Strefa C2",
-                                        "sellerMinimalUnitPrice": 185,
-                                        "proposalUnitPrice": 275,
-                                        "proposalZoneCode": "Strefa C2 od CEZ"
-                                    }],
-                                    "receiverPointEstimation": {
-                                        "sellerCode": "Cez",
-                                        "receiverPointDataEstimation": {
-                                            "tariffIssueDate": 1473325469351,
-                                            "estimatedUnitConsumptionInYearScale": 9999,
-                                            "estimatedContractProfitValue": 5000,
-                                            "estimatedContractProfitValueInYearScale": 2500,
-                                            "estimatedSavingsInYearScale": 600,
-                                            "estimatedSavingsInContractScale": 1200,
-                                            "estimatedSavingsInPercentage": 30
-                                        },
-                                        "receiverPointProvisionList": [{
-                                            "levelCode": "Próg I",
-                                            "traderProvisionInYearScale": 350,
-                                            "traderProvisionInContractScale": 700,
-                                            "partnerProvisionInContractScale": 175
-                                        }, {
-                                            "levelCode": "Próg II",
-                                            "traderProvisionInYearScale": 450,
-                                            "traderProvisionInContractScale": 900,
-                                            "partnerProvisionInContractScale": 225
-                                        }, {
-                                            "levelCode": "Próg III",
-                                            "traderProvisionInYearScale": 550,
-                                            "traderProvisionInContractScale": 1100,
-                                            "partnerProvisionInContractScale": 275
-                                        }]
-                                    }
-                                }]
-                            }
-                        }
-                    ],
-                    "allReceiverPointsOfferCalculation": null,
-                    "offerSummaryDTO": {
-                        "receiverPointEstimationList": [{
-                            "sellerCode": "TAURON",
-                            "allReceiverPointsDataEstimationForSeller": {
-                                "tariffIssueDate": 1473325469351,
-                                "estimatedContractValueForAllPoint": 5000,
-                                "estimatedContractValueInYearScaleForAllPoint": 2500,
-                                "estimatedSavingsInYearScaleForAllPoint": 600,
-                                "estimatedSavingsInContractScaleForAllPoint": 1200,
-                                "estimatedSavingsInPercentageForAllPoint": 30
-                            },
-                            "allReceiverPointsProvisionForSellerList": [{
-                                "levelCode": "Próg I",
-                                "traderProvisionInYearScaleForAllPoint": 300,
-                                "traderProvisionInContractScaleForAllPoint": 600,
-                                "partnerProvisionInContractScaleForAllPoint": 150
-                            }, {
-                                "levelCode": "Próg II",
-                                "traderProvisionInYearScaleForAllPoint": 400,
-                                "traderProvisionInContractScaleForAllPoint": 800,
-                                "partnerProvisionInContractScaleForAllPoint": 200
-                            }, {
-                                "levelCode": "Próg III",
-                                "traderProvisionInYearScaleForAllPoint": 500,
-                                "traderProvisionInContractScaleForAllPoint": 1000,
-                                "partnerProvisionInContractScaleForAllPoint": 250
-                            }]
-                        }, {
-                            "sellerCode": "CEZ",
-                            "allReceiverPointsDataEstimationForSeller": {
-                                "tariffIssueDate": 1473325469352,
-                                "estimatedContractValueForAllPoint": 2500,
-                                "estimatedContractValueInYearScaleForAllPoint": 1250,
-                                "estimatedSavingsInYearScaleForAllPoint": 300,
-                                "estimatedSavingsInContractScaleForAllPoint": 600,
-                                "estimatedSavingsInPercentageForAllPoint": 30
-                            },
-                            "allReceiverPointsProvisionForSellerList": [{
-                                "levelCode": "Próg I",
-                                "traderProvisionInYearScaleForAllPoint": 350,
-                                "traderProvisionInContractScaleForAllPoint": 700,
-                                "partnerProvisionInContractScaleForAllPoint": 175
-                            }, {
-                                "levelCode": "Próg II",
-                                "traderProvisionInYearScaleForAllPoint": 450,
-                                "traderProvisionInContractScaleForAllPoint": 900,
-                                "partnerProvisionInContractScaleForAllPoint": 225
-                            }, {
-                                "levelCode": "Próg III",
-                                "traderProvisionInYearScaleForAllPoint": 550,
-                                "traderProvisionInContractScaleForAllPoint": 1100,
-                                "partnerProvisionInContractScaleForAllPoint": 275
-                            }]
-                        }]
-                    },
-                    "offerNote": "Estymacja może ulec zmianie w przypadku zmian cen sprzedawcy energii",
-                    "offerCalculationPerReceiverPointSet": true
-                },
-              headers: {
-                  'Content-Type': 'application/json'
-              }
-            }).success(function(response){
-                    $scope.response = response;
-                }).error(function(error){
-                    $scope.error = error;
-                });
+                method: 'POST',
+                dataType: 'json',
+                url: 'https://easy-energy.herokuapp.com/a/electricityOffer.json',
+                data: $scope.content,
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).success(function(response) {
+                $scope.response = response;
+            }).error(function(error) {
+                $scope.error = error;
+            });
             convertDate();
         };
 
         $scope.sendCalculation = function() {
 
-          convertDateFromObject();
+            convertDateFromObject();
 
-          $http(reqPostCalculation).success(function(response){
-                  $scope.response = response;
-              }).error(function(error){
-                  $scope.error = error;
-              });
-          convertDate();
-
+            $http({
+                method: 'POST',
+                dataType: 'json',
+                url: 'https://easy-energy.herokuapp.com/a/CalculateElectricityOffer',
+                data: $scope.content,
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).success(function(response) {
+                $scope.response = response;
+            }).error(function(error) {
+                $scope.error = error;
+            });
+            convertDate();
 
         };
 
@@ -395,7 +151,6 @@ angular.module('myApp', ['angularModalService', 'ngAnimate'])
             }
 
         };
-
 
         $scope.addPunctItem = function() {
             if ($scope.content.receiverPointList.length == 0) {
@@ -734,4 +489,4 @@ angular.module('myApp', ['angularModalService', 'ngAnimate'])
             $scope.disableCalculationOffer = false;
         };
 
-      }]);
+    }]);
