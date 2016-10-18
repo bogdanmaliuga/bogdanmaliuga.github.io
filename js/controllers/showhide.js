791d8410624d0162044f0eddc5838006d17009ef
bogdanmaliuga <bogdan.maliuga@gmail.com>
2016-10-18 16:06:55 +0300

    s

--- js/controllers/showhide.js
+++ js/controllers/showhide.js
@@ -2,16 +2,16 @@ var app = angular.module('myApp');
 
 app.controller('ShowHideController', ['$scope', function($scope) {
     var vm = this;
-    vm.status='ukryj';
+    vm.status='glyphicon-minus';
     vm.isHide = false;
     vm.showhide = function(index) {
     	if(vm.isHide==false){
     		vm.isHide=true;
-    		vm.status='pokaż'
+    		vm.status='glyphicon-plus-sign'
     	}
     	else{
     		vm.isHide=false;
-    		vm.status='ukryj';
+    		vm.status='glyphicon-minus';
     	}
     }
 
